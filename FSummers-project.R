30f710a400f0006bd6da503f411317cc3c797b42
FrancesCode24 <summers.frances@gmail.com>
2026-03-17 23:47:33 -0400

    added RDS data and updated script

--- FSummers-project.R
+++ FSummers-project.R
@@ -13,12 +13,18 @@ library(tidyverse)
 library(janitor)
 
 fbi_lee <- read_csv("~/Desktop/R class materials/community policing/data cp/fbi_lee_virginia.csv")
+saveRDS(fbi_lee, "data/fbi_lee.RDS")
+readRDS("data/fbi_lee.RDS")
 
 lee_1960_2024 <- read_csv("~/Desktop/R class materials/community policing/data cp/lee_1960_2024.csv")
+saveRDS(lee_1960_2024, "data/lee_1960_2024.RDS")
+readRDS("data/lee_1960_2024.RDS")
 
-vacpd <- read_csv("~/Desktop/R class materials/community policing/data cp/vacpd_20260225.csv") %>%
-  clean_names 
+vacpd <- read_csv("~/Desktop/R class materials/community policing/data cp/vacpd_20260225.csv")
+saveRDS(vacpd, "data/vacpd.RDS")
+readRDS("data/vacpd.RDS")
 
+#vacpd data----
 glimpse(vacpd)
 
 vacpd <- vacpd %>%
@@ -33,7 +39,17 @@ vacpd %>%
     force_by_officer = sum(FORCE.USED.BY.OFFICER == "Y", na.rm = TRUE), 
     force_rate = FORCE.USED.BY.OFFICER / tot_stops * 100)
 
-glimpse(vacpd)
+#lee_1960_2024 data----
+
+#Question
+
+#fbi_lee data----
+
+#Question
+
+
+
+
 
 
 
