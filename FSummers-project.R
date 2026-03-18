478e2b3c1470cf3552b0783124b7e6d75885261a
FrancesCode24 <summers.frances@gmail.com>
2026-03-17 23:22:19 -0400

    loaded data and looked at stops per year

---
+++ FSummers-project.R
@@ -0,0 +1,38 @@
+#Community Policing Data 
+#Team Berries
+#Date: March 2026
+
+#Start here----
+#Project Objective----
+# To analyze ... 
+
+
+
+#Load the data----
+library(tidyverse)
+library(janitor)
+
+fbi_lee <- read_csv("~/Desktop/R class materials/community policing/data cp/fbi_lee_virginia.csv")
+
+lee_1960_2024 <- read_csv("~/Desktop/R class materials/community policing/data cp/lee_1960_2024.csv")
+
+vacpd <- read_csv("~/Desktop/R class materials/community policing/data cp/vacpd_20260225.csv") %>%
+  clean_names 
+
+glimpse(vacpd)
+
+vacpd <- vacpd %>%
+  mutate(date = mdy(STOP_DATE), 
+         year = year(date))
+
+#How many stops and force used by officer per year statewide?
+vacpd %>%
+  group_by(year) %>%
+  summarize(
+    tot_stops = n(), 
+    force_by_officer = sum(FORCE.USED.BY.OFFICER == "Y", na.rm = TRUE), 
+    force_rate = FORCE.USED.BY.OFFICER / tot_stops * 100)
+
+
+
+
